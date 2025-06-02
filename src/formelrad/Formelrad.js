ab5c774e61b8e8d9f78381891ed50e559d5d8633
lorenzzhh <lorenz.fritschi@gmx.ch>
2025-06-02 16:12:43 +0200

    0013-Handler-has-to-react-on-submit

--- src/formelrad/Formelrad.js
+++ src/formelrad/Formelrad.js
@@ -9,6 +9,12 @@ export default function Formelrad() {
         r: "",
         p: ""
     })
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log("handleSubmit")
+    }
+
     return (
         <>
             <section>
@@ -16,7 +22,7 @@ export default function Formelrad() {
                     <h2>Formelrad</h2>
                     <img src={formelrad} width="200" alt="Formelrad"/>
                 </header>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <InputField color={"black"} value={values.u} label="Spannung" handleChange={e => {setValues(values => ({...values, u: e.target.value}))}} />
                     <InputField color={"black"} value={values.i} label="Stromstärke" handleChange={e => {setValues(values => ({...values, i: e.target.value}))}} />
                     <InputField color={"black"} value={values.r} label="Widerstand" handleChange={e => {setValues(values => ({...values, r: e.target.value}))}} />
